d7449934f96b3e744290202abac772bf98e96b2d
Sandrine Brival <sandrine.brival@octo.com>
2022-09-21 12:30:01 +0200

    test env add

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const port = process.env.PORT
+const test = process.env.TEST
 
 app.get("/", function (req, res) {
-  res.send(`Hello World!`)
+  res.send(`Hello World! >> ${test}`)
 })
 
-const port = process.env.PORT
+
 app.listen(port, function () {
   console.log(`ToDo API listening on port ${port}`)
 })
